fd6cf421ae2ccf37dc2d8028bcedd089391ac125
spdowl135 <stephen.p.dowling@gmail.com>
2015-11-15 23:11:04 -0500

    Add many changes
    Changes

--- www/mygroups.php
+++ www/mygroups.php
@@ -1,6 +1,12 @@
+<?php
+include("functions2.php");
+?>
 <h2>My Active Groups</h2>
-<?php 
-$query2 = mysql_query("SELECT * FROM groups WHERE grpusername='$userdbme' AND grpactive='Yes' AND grpaccept='Yes' ORDER BY grpid DESC");
+<nav id="primary_nav">
+	<ul>
+<?php
+//Use $userdbme where thestevedowling is being used 
+$query2 = mysql_query("SELECT * FROM groups WHERE grpusername='thestevedowling' AND grpactive='Yes' AND grpaccept='Yes' ORDER BY grpid DESC");
 while($row2 = mysql_fetch_assoc($query2))
 {
 $grpidact = $row2['grpid'];
@@ -9,12 +15,17 @@ $grpname = $row2['groupname'];
 $grpname = stripslashes($grpname);
 $viewmore = "View More";
 //echo '<a href="http://www.bunchofus.com/mobile/group/' . $grpidact . '">' . $grpname . '</a><br />';
-echo "$grpname<br />";
+echo "<li><a href='#'>$grpname</a></li>";
 }
 ?>
+</ul>
+</nav>
+<div class="clear"></div>
 <h2>Active Joined Groups</h2>
+<nav id="primary_nav">
+	<ul>
 <?php 
-$query2a = mysql_query("SELECT DISTINCT grpid FROM groups JOIN ingroup WHERE ingroup.inusername = '$userdbme' AND groups.grpactive = 'Yes' AND groups.grpid = ingroup.ingroupid AND groups.grpusername != ingroup.inusername ORDER BY groups.grpid DESC");
+$query2a = mysql_query("SELECT DISTINCT grpid FROM groups JOIN ingroup WHERE ingroup.inusername = 'thestevedowling' AND groups.grpactive = 'Yes' AND groups.grpid = ingroup.ingroupid AND groups.grpusername != ingroup.inusername ORDER BY groups.grpid DESC");
 while($row2a = mysql_fetch_assoc($query2a))
 {
 $grpidact2a = $row2a['grpid'];
@@ -27,6 +38,8 @@ $grpname2aa = $row2aa['groupname'];
 $grpname2aa = stripslashes($grpname2aa);
 $viewmore = "View More";
 //echo '<a href="http://www.bunchofus.com/mobile/group/' . $grpidact2a . '">' . $grpname2aa . '</a><br />';
-echo "$grpname2aa<br />";
+echo "<li><a href='#'>$grpname2aa</a></li>";
 }}
 ?>
+</ul>
+</nav>
